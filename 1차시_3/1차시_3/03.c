fa8103257bcbecb887550082419ce804988f4a78
변지원 <jwbyeon0524@gmail.com>
2026-03-30 01:43:07 +0900

    feat: Haedal 프로젝트 추가

---
+++ 1차시_3/1차시_3/03.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	int i, n;
+
+	scanf("%d", &n);
+
+	for (i = 1; i < 10; i++)
+		printf("%d * %d = %d\n", n, i, n * i);
+	
+
+	return 0;
+}
